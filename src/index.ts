34c65ea9bc706816055d784ede1efbf3c2632831
Villalpando Julien <villalpando.julien@gmail.com>
2022-02-16 16:30:42 +0100

    En cours button/page et button/return

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import apiCall from "@fewlines-education/request";
-import { cp } from "fs";
 
 const app = express();
 app.use(express.static("Public"));
@@ -15,12 +14,13 @@ app.get("/", (request, response) => {
     }
     const data = JSON.parse(body);
 
-    response.render("home", { platforminfo: data, platform: data.platforms });
+    response.render("plateform", { platforminfo: data, platform: data.platforms });
   });
 });
-app.get("/Games/:itemid", (request, response) => {
+app.get("/Games/:itemname/:itemid", (request, response) => {
   const routeParameters = request.params;
   const idplateform = routeParameters.itemid;
+  const idname = routeParameters.itemname;
 
   apiCall(`http://videogame-api.fly.dev/games/platforms/${idplateform}`, (error, body) => {
     if (error) {
@@ -28,11 +28,11 @@ app.get("/Games/:itemid", (request, response) => {
     }
     const data = JSON.parse(body);
 
-    response.render("games", { listGame: data.games });
+    response.render("games", { nameid: idname, listGame: data.games });
   });
 });
 
-app.get("/Games/game/:itemid", (request, response) => {
+app.get("/Games/game/:itemname/:itemid", (request, response) => {
   const routeParameters = request.params;
   const idgame = routeParameters.itemid;
 
@@ -41,9 +41,8 @@ app.get("/Games/game/:itemid", (request, response) => {
       throw error;
     }
     const data = JSON.parse(body);
-    console.log(data);
 
-    response.render("descriptiongame", { gameinfo: data, gamegenres: data.genres, gamescreenshots: data.screenshots });
+    response.render("game", { gameinfo: data, gamegenres: data.genres, gamescreenshots: data.screenshots });
   });
 });
 
