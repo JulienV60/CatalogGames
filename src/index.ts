d40fcea300abbaff4ead2116ab124465db2b1378
Villalpando Julien <villalpando.julien@gmail.com>
2022-02-16 13:57:53 +0100

    En cours button return homepage et button page1,2,3,4,etc

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,45 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import apiCall from "@fewlines-education/request";
+
 const app = express();
 app.use(express.static("Public"));
 nunjucks.configure("views", { autoescape: true, express: app });
 app.set("view engine", "njk");
 
 app.get("/", (request, response) => {
-  apiCall("http://videogame-api.fly.dev/games", (error, body) => {
+  apiCall("http://videogame-api.fly.dev/platforms", (error, body) => {
     if (error) {
       throw error;
     }
-    const joke = JSON.parse(body);
-    console.log(joke);
-    response.render("home");
+    const data = JSON.parse(body);
+    response.render("home", { platform: data.platforms });
   });
 });
+app.get("/Games/:itemid", (request, response) => {
+  const routeParameters = request.params;
+  const id = routeParameters.itemid;
 
+  apiCall(`http://videogame-api.fly.dev/games/platforms/${id}`, (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const data = JSON.parse(body);
+    response.render("games", { listGame: data.games });
+  });
+});
+app.get("/Games/game/:itemid", (request, response) => {
+  const routeParameters = request.params;
+  const id = routeParameters.itemid;
+
+  apiCall(`http://videogame-api.fly.dev/games/${id}`, (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const data = JSON.parse(body);
+    response.render("descriptiongame", { game: data.screenshots });
+  });
+});
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });
